7de9bff55f6f98f9a60170eeecf163fff94cc7a4
Adam Paweł Łuczak <a.luczak2005@gmail.com>
2025-11-20 11:04:14 +0100

    Fix variable name in cosine_similarity function

--- vectorizer.py
+++ vectorizer.py
@@ -8,7 +8,7 @@ def encode_item(metadata: Dict) -> Vector:
 def cosine_similarity(a: Vector, b: Vector) -> float:
   dot = sum(x*y for x,y in zip(a,b))
   na = sum(x*x for x in a) ** (0.5)
-  nb = sum(x*x for y in b) ** (0.5)
+  nb = sum(y*y for y in b) ** (0.5)
   if na == 0 or nb == 0:
     return 0.0
   return dot/(na*nb)
